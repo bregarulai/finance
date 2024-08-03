bf9362a40ccf0f1624290073887d97c038969eaa
bregarulai <bregarulai@gmail.com>
2024-08-03 16:33:22 -0400

    Implement auth routes, move home page to dashboard group routes

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>get started</Button>
     </main>
   );
-}
+};
+
+export default Home;
