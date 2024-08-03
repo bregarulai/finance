ac34b3562ba04eeb63d5582afc8926bdfc74d089
bregarulai <bregarulai@gmail.com>
2024-08-03 17:42:01 -0400

    Implement signInPage

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,31 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 const SignInPage = () => {
-  return <SignIn />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center">
+        <div className="text-center pace-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Login or Create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden lg:flex items-center justify-center h-full bg-blue-600">
+        <Image src="/assets/logo.svg" alt="logo" width={100} height={100} />
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
