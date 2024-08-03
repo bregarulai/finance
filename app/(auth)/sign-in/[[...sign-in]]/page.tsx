77a1f79cc50f4aca777850ab99de8ca8f533e38c
bregarulai <bregarulai@gmail.com>
2024-08-03 18:03:35 -0400

    implement signUpPage

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -14,7 +14,7 @@ const SignInPage = () => {
         </div>
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignIn />
+            <SignIn path="/sign-in" />
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground" />
