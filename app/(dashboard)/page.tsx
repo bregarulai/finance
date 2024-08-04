b88f4772b6856d535a72b7af735283520f6db2be
bregarulai <bregarulai@gmail.com>
2024-08-04 17:50:14 -0400

    Implement title and add new button on accounts page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
-
 const Home = () => {
-  const { onOpen } = useNewAccount();
   return (
     <div>
       <p>Home Dashboard</p>
-      <Button onClick={onOpen}>Add an account</Button>
     </div>
   );
 };
