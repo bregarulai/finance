98296e74260e4c7ad2458165c5e64cb5f18108d4
bregarulai <bregarulai@gmail.com>
2024-08-03 22:52:33 -0400

    Implement mobile navigation drawer and links

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,12 +1,52 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useMedia } from "react-use";
+import { Menu } from "lucide-react";
 
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { routes } from "@/constants";
 import NavButton from "./NavButton";
+import { Button } from "./ui/button";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
   const pathname = usePathname();
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const handleClose = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button className="shad-btn-menu" variant="outline" size="sm">
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="px-2" side="left">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                key={route.href}
+                className="full-width justify-start"
+                variant={route.href === pathname ? "secondary" : "ghost"}
+                onClick={() => handleClose(route.href)}
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
       {routes.map((route) => (
