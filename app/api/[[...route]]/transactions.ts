9bc0a9c86d2caa92769eed4ef9e3968502a7cf0c
bregarulai <bregarulai@gmail.com>
2024-08-06 00:16:44 -0400

    Implement get all transactions endpoint

---
+++ app/api/[[...route]]/transactions.ts
@@ -0,0 +1,73 @@
+import { Hono } from "hono";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { db } from "@/db/drizzle";
+import { and, desc, eq, gte, inArray, lte } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
+import { parse, subDays } from "date-fns";
+
+import {
+  transactions,
+  insertTransactionSchema,
+  categories,
+  accounts,
+} from "@/db/schema";
+
+const app = new Hono().get(
+  "/",
+  zValidator(
+    "query",
+    z.object({
+      from: z.string().optional(),
+      to: z.string().optional(),
+      accountId: z.string().optional(),
+    })
+  ),
+  clerkMiddleware(),
+  async (c) => {
+    const auth = getAuth(c);
+    const { from, to, accountId } = c.req.valid("query");
+
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    const defaultTo = new Date();
+    const defaultFrom = subDays(defaultTo, 30);
+
+    const startDate = from
+      ? parse(from, "yyyy-MM-dd", new Date())
+      : defaultFrom;
+    const endDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+
+    const data = await db
+      .select({
+        id: transactions.id,
+        date: transactions.date,
+        category: categories.name,
+        categoryId: transactions.categoryId,
+        payee: transactions.payee,
+        amount: transactions.amount,
+        notes: transactions.notes,
+        account: accounts.name,
+        accountId: transactions.accountId,
+      })
+      .from(transactions)
+      .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+      .leftJoin(categories, eq(transactions.categoryId, categories.id))
+      .where(
+        and(
+          accountId ? eq(transactions.accountId, accountId) : undefined,
+          eq(accounts.userId, auth.userId),
+          gte(transactions.date, startDate),
+          lte(transactions.date, endDate)
+        )
+      )
+      .orderBy(desc(transactions.date));
+
+    return c.json({ data });
+  }
+);
+
+export default app;
