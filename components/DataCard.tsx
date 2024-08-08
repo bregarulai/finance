9821ec7b57e3815bcf32488eaf8b6427b21e6201
bregarulai <bregarulai@gmail.com>
2024-08-08 12:04:56 -0400

    Implement header section for DataCard component

---
+++ components/DataCard.tsx
@@ -0,0 +1,89 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { IconType } from "react-icons";
+
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+const boxVariant = cva("shrink-0 rounded-md p-3", {
+  variants: {
+    variant: {
+      default: "bg-blue-500/20",
+      success: "bg-emerald-500/20",
+      danger: "bg-rose-500/20",
+      warning: "bg-yellow-500/20",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+const iconVariant = cva("size-6", {
+  variants: {
+    variant: {
+      default: "fill-blue-500",
+      success: "fill-emerald-500",
+      danger: "fill-rose-500",
+      warning: "fill-yellow-500",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+type BoxVariants = VariantProps<typeof boxVariant>;
+type IconVariants = VariantProps<typeof iconVariant>;
+
+interface DataCardProps extends BoxVariants, IconVariants {
+  title: string;
+  value?: number;
+  percentageChange?: number;
+  icon: IconType;
+  dateRange: string;
+}
+
+const DataCard = ({
+  title,
+  value = 0,
+  percentageChange = 0,
+  icon: Icon,
+  dateRange,
+  variant,
+}: DataCardProps) => {
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="space-y-2">
+          <CardTitle className="text-2xl line-clamp-1">{title}</CardTitle>
+          <CardDescription className="line-clamp-1">
+            {dateRange}
+          </CardDescription>
+        </div>
+        <div
+          className={cn(
+            boxVariant({
+              variant,
+            })
+          )}
+        >
+          <Icon
+            className={cn(
+              iconVariant({
+                variant,
+              })
+            )}
+          />
+        </div>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default DataCard;
--- components/DataGrid.tsx
+++ components/DataGrid.tsx
@@ -1,15 +1,32 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+
+import { formatDateRange } from "@/lib/utils";
+import { useGetSummary } from "@/features/summary/api/useGetSummary";
+import DataCard from "./DataCard";
 
 const DataGrid = () => {
+  const { data } = useGetSummary();
+
   const params = useSearchParams();
   const to = params.get("to") || undefined;
   const from = params.get("from") || undefined;
+  const parsedTo = parseFloat(to!);
+
+  const dateRangeLabel = formatDateRange({ to: parsedTo, from: from });
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-      DataGrid
+      <DataCard
+        title="Remaining"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
     </div>
   );
 };
