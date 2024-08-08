aa795ea6d532f271ffb9f2ef85db655a1698fe5e
bregarulai <bregarulai@gmail.com>
2024-08-08 14:22:38 -0400

    Implement income and expenses card, loading skeleton card

--- components/DataCard.tsx
+++ components/DataCard.tsx
@@ -10,6 +10,7 @@ import {
   CardTitle,
 } from "./ui/card";
 import { CountUp } from "./CountUp";
+import { Skeleton } from "./ui/skeleton";
 
 const boxVariant = cva("shrink-0 rounded-md p-3", {
   variants: {
@@ -109,3 +110,21 @@ const DataCard = ({
 };
 
 export default DataCard;
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm h-[193px]">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="space-y-2">
+          <Skeleton className="h-6 w-24" />
+          <Skeleton className="h-4 w-40" />
+        </div>
+        <Skeleton className="size-12" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="shrink-0 h-10 w-24 mb-2" />
+        <Skeleton className="shrink-0 h-4 w-40" />
+      </CardContent>
+    </Card>
+  );
+};
--- components/DataGrid.tsx
+++ components/DataGrid.tsx
@@ -2,13 +2,14 @@
 
 import { useSearchParams } from "next/navigation";
 import { FaPiggyBank } from "react-icons/fa";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 
 import { formatDateRange } from "@/lib/utils";
 import { useGetSummary } from "@/features/summary/api/useGetSummary";
-import DataCard from "./DataCard";
+import DataCard, { DataCardLoading } from "./DataCard";
 
 const DataGrid = () => {
-  const { data } = useGetSummary();
+  const { data, isLoading } = useGetSummary();
 
   const params = useSearchParams();
   const to = params.get("to") || undefined;
@@ -17,6 +18,16 @@ const DataGrid = () => {
 
   const dateRangeLabel = formatDateRange({ to: parsedTo, from: from });
 
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+        <DataCardLoading />
+        <DataCardLoading />
+        <DataCardLoading />
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
       <DataCard
@@ -24,6 +35,22 @@ const DataGrid = () => {
         value={data?.remainingAmount}
         percentageChange={data?.remainingChange}
         icon={FaPiggyBank}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Income"
+        value={data?.incomeAmount}
+        percentageChange={data?.incomeChange}
+        icon={FaArrowTrendUp}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Expenses"
+        value={data?.expensesAmount}
+        percentageChange={data?.expensesChange}
+        icon={FaArrowTrendDown}
         variant="default"
         dateRange={dateRangeLabel}
       />
