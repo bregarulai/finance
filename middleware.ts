1610a8c015978ab2c21e07c4bcc2f38abfcc35bb
bregarulai <bregarulai@gmail.com>
2024-08-09 00:55:42 -0400

    Implement routes matcher

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/transactions(.*)",
+  "/categories(.*)",
+  "/accounts(.*)",
+  "/settings(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
